cf9d8d9e502a9840527f88eae34884b097c65300
eztree <huienlim@gmail.com>
2021-11-26 08:56:29 +0800

    Push to clear

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,12 @@
+class BookmarksController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+    # redirect_to bookmark_path
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Initializing the seeds 🌱"
+
+10.times do
+  puts "Creating a movie now... 📽"
+  title = Faker::Movie.title
+  overview = Faker::Movie.quote
+  poster_url = 'https://source.unsplash.com/random/400x300'
+  rating = rand(1.0..10.0).round(1)
+  Movie.create!(title: title, overview: overview, poster_url: poster_url, rating: rating)
+  puts "Movie created!"
+end
+
+puts "End of seeds 🙌"
